bedfac7dfc2ef4333d400535a35df328f0aa88d2
@slovacus <slovacus@gmail.com>
2017-08-22 22:10:38 -0500

    !#3 Add: validate code wercker

--- script/run.sh
+++ script/run.sh
@@ -5,3 +5,27 @@
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
 
 cd "${ROOT_DIR}" || echo 'Not Found'
+
+if [ ! -n "$WERCKER_TOKEN" ]; then
+  fail 'Please specify Token'
+fi
+
+if [ ! -n "$WERCKER_GROUP" ]; then
+  fail 'Please specify Group'
+fi
+
+if [ "$WERCKER_WORKPLACE_NOTIFY_ON" = "failed" ]; then
+  if [ "$WERCKER_RESULT" = "passed" ]; then
+    echo "Skipping.."
+    return 0
+  fi
+fi
+
+if [ -n "$WERCKER_WORKPLACE_NOTIFY_BRANCH" ]; then
+  if [ "$WERCKER_GIT_BRANCH" != "$WERCKER_WORKPLACE_NOTIFY_BRANCH" ]; then
+    echo "Workplace notification not necessary on this branch. Skipping.."
+    return 0
+  fi
+fi
+
+python "${SOURCE_DIR}/run.py"
