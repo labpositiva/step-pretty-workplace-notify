c377aca5d094b6da6cfe74f593b5decfe25d7d14
@slovacus <slovacus@gmail.com>
2017-08-23 01:23:05 -0500

    !#3 Add: only test command

--- script/run.sh
+++ script/run.sh
@@ -6,12 +6,14 @@
 
 cd "${ROOT_DIR}" || echo 'Not Found'
 
+export COMMAND="${2//\"/}"
+
 if [ "$1" == "" ]; then
     docker-compose up --remove-orphans
 elif [ "$1" == "dev" ]; then
-    docker-compose -f docker-compose.yml -f docker-compose/dev.yml -p "${PROJECT_NAME_DEV}" run --rm app "${2}"
+    docker-compose -f docker-compose.yml -f docker-compose/dev.yml -p "${PROJECT_NAME_DEV}" run --rm app "${COMMAND}"
 elif [ "$1" == "test" ]; then
-    docker-compose -f docker-compose.yml -f docker-compose/test.yml -p "${PROJECT_NAME_TEST}" run --rm app "${2}"
+    docker-compose -f docker-compose.yml -f docker-compose/test.yml -p "${PROJECT_NAME_TEST}" run --rm app "${COMMAND}"
 elif [ "$1" == "prod" ]; then
-    docker-compose -f docker-compose.yml -f docker-compose/prod.yml -p "${PROJECT_NAME}" run --rm app "${2}"
+    docker-compose -f docker-compose.yml -f docker-compose/prod.yml -p "${PROJECT_NAME}" run --rm app "${COMMAND}"
 fi
